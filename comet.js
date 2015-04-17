ae7db0e4d75629d9f1028a8e5522a910a71e9626
Vinicius Michelutti <vinicius.michelutti@fluig.com>
2015-04-17 18:21:20 -0300

    Login style

--- comet.js
+++ comet.js
@@ -1,17 +1,10 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+  Template.login.helpers({
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.login.events({
+    'submit form': function () {
     }
   });
 }

76ae6d797e25d31702197fe33ab6970be002842f
Vinicius Michelutti <vinicius.michelutti@fluig.com>
2015-06-09 14:56:11 -0300

    Close #13 - Auto scroll for all users

--- client/js/views/messages.js
+++ client/js/views/messages.js
@@ -2,6 +2,11 @@ Template.messages.onCreated(function() {
   var self = this;
   self.autorun(function() {
     self.subscribe('messages', Session.get('channel'));
+
+    var msgs = Messages.find().count();
+    if ($('.messages')[0]) {
+      $('.messages').scrollTop($('.messages')[0].scrollHeight);
+    }
   });
 });
 
